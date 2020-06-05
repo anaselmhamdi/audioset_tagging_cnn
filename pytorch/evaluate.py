01f879b88d4064c447ea05494e0285c1748700f3
Anas El Mhamdi <anas.elmhamdi@gmail.com>
2020-06-06 01:16:19 +0200

    did litteraly stuff

--- pytorch/evaluate.py
+++ pytorch/evaluate.py
@@ -28,7 +28,7 @@ class Evaluator(object):
             generator=self.generator, 
             return_target=True)
 
-        clipwise_output = output_dict['clipwise_output']    # (audios_num, classes_num)
+        clipwise_output = output_dict['framewise_output']    # (audios_num, classes_num)
         target = output_dict['target']    # (audios_num, classes_num)
 
         average_precision = metrics.average_precision_score(
