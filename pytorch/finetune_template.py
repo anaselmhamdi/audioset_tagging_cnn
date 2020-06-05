b9d4e84af0059e04c66a1bf6cd7cecd42e462974
Anas El Mhamdi <anas.elmhamdi@gmail.com>
2020-06-05 16:33:00 +0200

    added cpu gpu handling, readded clipwise output

--- pytorch/finetune_template.py
+++ pytorch/finetune_template.py
@@ -105,6 +105,7 @@ class Transfer_Cnn14_DecisionLevelAtt(nn.Module):
         """Input: (batch_size, data_length)
         """
         output_dict = self.base(input, mixup_lambda)
+        embedding = output_dict['embedding']
         clipwise_output =  torch.log_softmax(self.fc_transfer(embedding), dim=-1)
         output_dict['clipwise_output'] = clipwise_output
         return output_dict
