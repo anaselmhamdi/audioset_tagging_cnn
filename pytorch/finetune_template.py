15a251845b133581e81e5e90df0a39c0006dc0d1
Anas El Mhamdi <anas.elmhamdi@gmail.com>
2020-06-05 18:13:32 +0200

    did litteraly stuff

--- pytorch/finetune_template.py
+++ pytorch/finetune_template.py
@@ -105,7 +105,7 @@ class Transfer_Cnn14_DecisionLevelAtt(nn.Module):
         """
         output_dict = self.base(input, mixup_lambda)
         x = output_dict['clipwise_output']
-        framewise_output = torch.log_softmax(self.fc_transfer(x), dim=-1)
+        framewise_output = torch.log_softmax(self.fc_transfer(x.transpose(1, 2)), dim=-1)
         output_dict['framewise_output'] = framewise_output
         return output_dict
 
